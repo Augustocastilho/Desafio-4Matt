3ba216b97597cf10ed3d398adb99335270832e34
Augusto Castilho <castilho@ice.ufjf.br>
2023-10-26 02:47:00 -0300

    Implementação de busca e alteração do status no BD

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,89 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+const mysql = require('mysql2');
 
-app.get('/', (req, res) => {
-  res.send('Olá, mundo!');
+app.use(cors());
+app.use(express.json());
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'Desafio-4Matt',
 });
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+connection.connect((err) => {
+  if (err) throw err;
 });
+
+
+function convertStatus(status) {
+  if (status === 0)
+    return 'Parado';
+  return 'Funcionando';
+}
+
+function unconvertStatus(status) {
+  if (status === 'Parado')
+    return 0;
+  return 1;
+}
+
+function executeQuery(sql, filtro) {
+  return new Promise((resolve, reject) => {
+    connection.query(sql, filtro, (error, results, fields) => {
+      if (error) throw error;
+      for (let i = 0; i < results.length; i++) {
+        results[i].status = convertStatus(results[i].status);
+      }
+      resolve(results);
+    });
+  });
+}
+
+app.get('/getTableData', (req, res) => {
+  const { ic } = req.query;
+
+  let sql = 'SELECT * FROM processo';
+  let filtro = [];
+
+  if (ic) {
+    sql += ' WHERE ic = ?';
+    filtro.push(ic);
+  }
+
+  executeQuery(sql, filtro)
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.patch('/alterarStatus', (req, res) => {
+  const { nome } = req.query;
+
+  const processo = 'SELECT * FROM processo WHERE nome = ?';
+  executeQuery(processo, [nome])
+    .then((results) => {
+      const status = unconvertStatus(results[0].status);
+      const novoStatus = !status;
+
+      const sql = 'UPDATE processo SET status = ? WHERE nome = ?';
+
+      executeQuery(sql, [novoStatus, nome])
+        .then(() => {
+          res.json({ message: 'Status alterado com sucesso!' });
+        })
+        .catch(() => {
+          res.json({ message: 'Erro ao alterar status!' });
+        });
+    })
+    .catch(() => {
+      res.json({ message: 'Erro ao alterar status!' });
+    });
+});
+
+app.listen(3000);
